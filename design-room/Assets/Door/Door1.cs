05981f650a5d5d65970ecf34968f15eb7d2e2e23
NewLuminous <newluminous7@gmail.com>
2020-11-21 19:41:45 +0700

    Fix script for animating Door1

---
+++ design-room/Assets/Door/Door1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door1 : MonoBehaviour
+{
+    private bool isOpened = false;
+    private Animation parentAnimation;
+    private Collider m_collider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentAnimation = transform.parent.parent.GetComponent<Animation>();
+        m_collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (isOpened)
+        {
+            if (m_collider.name.Equals("Door"))
+            {
+                parentAnimation.PlayQueued("CloseRight");
+            } else {
+                parentAnimation.PlayQueued("CloseLeft");
+            }
+            isOpened = false;
+        } else {
+            if (m_collider.name.Equals("Door"))
+            {
+                parentAnimation.PlayQueued("OpenRight");
+            } else {
+                parentAnimation.PlayQueued("OpenLeft");
+            }
+            isOpened = true;
+        }
+    }
+}
