a2fb34c6628c71d05da7be114c4f5b7f53f36937
NewLuminous <newluminous7@gmail.com>
2020-11-21 19:29:27 +0700

    Animate Door1

---
+++ design-room/Assets/Door/Door1Left.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door1Left : MonoBehaviour
+{
+    private bool isOpened = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (isOpened)
+        {
+            transform.parent.parent.GetComponent<Animation>().PlayQueued("CloseLeft");
+            isOpened = false;
+        } else {
+            transform.parent.parent.GetComponent<Animation>().PlayQueued("OpenLeft");
+            isOpened = true;
+        }
+    }
+}
